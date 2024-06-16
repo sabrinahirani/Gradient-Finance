d8555afc09f426e7a85df27ac54c3ea41c671c5e
SabrinaHirani <sabrinahirani@gmail.com>
2024-06-15 20:41:07 -0400

    Rudimentary GradientSAVault.sol

--- contracts/contracts/GradientDebtVault.sol
+++ contracts/contracts/GradientDebtVault.sol
@@ -2,12 +2,27 @@
 pragma solidity ^0.8.0;
 
 import "../node_modules/solmate/src/mixins/ERC4626.sol";
+import "../node_modules/solmate/src/tokens/ERC20.sol"; // for DAI
+import "solmate/src/utils/ReentrancyGuard.sol";
 
-contract GradientDebtVault is ERC4626 {
+contract GradientDebtVault is ERC4626, ReentrancyGuard {
 
-    constructor(address dai) ERC4626(dai, "Gradient Collateral", "GRADC") {}
+    constructor(address dai) ERC4626(ERC20(dai), "Gradient Collateral", "GRADC") {}
 
     // TODO method for adjusting share based on price movements + ratios between GradientSAVault
+
+    /**
+     * @inheritdoc ERC4626
+     */
+    function totalAssets() public view override returns (uint256) {
+        assembly { // better safe than sorry
+            if eq(sload(0), 2) {
+                mstore(0x00, 0xed3ba6a6)
+                revert(0x1c, 0x04)
+            }
+        }
+        return asset.balanceOf(address(this));
+    }
 
     /**
      * @inheritdoc ERC4626
--- contracts/contracts/GradientSAVault.sol
+++ contracts/contracts/GradientSAVault.sol
@@ -1,47 +1,137 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// TODO review arithmetic + ownership
+
 import "../node_modules/solmate/src/mixins/ERC4626.sol";
-
 import "../node_modules/solmate/src/tokens/ERC20.sol"; // for GRADC token
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; // for price oracle
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol"; // for price oracle
 
-contract GradientSAVault is ERC4626 {
+import "solmate/src/utils/ReentrancyGuard.sol";
+
+contract GradientSAVault is ERC4626, ChainlinkClient, ReentrancyGuard {
+
+    using Chainlink for Chainlink.Request;
 
     uint256 public value;
 
-    ERC20 public collateral;
-    address public oracle;
-
     struct Asset {
-        string name;
+        string ticker;
         uint256 weight;
-        uint256 amount;
     }
+    Asset[] public underlying_assets;
 
-    Asset[] public assets;
+    mapping(string => uint256) underlying_asset_value;
+    mapping(string => uint256) underlying_asset_qty;
 
-    constructor(string memory _name, string memory _ticker, uint256 _value, address _collateral, address _oracle, address _minter, string[] memory _assets, uint[] memory _weights) ERC4626 (_collateral, _name, _ticker) {
+    mapping(bytes32 => string) query_asset;
+    bool underlying_asset_qty_is_empty;
+
+    event RequestMade(bytes32 indexed requestId);
+    event RequestProcessed(bytes32 indexed requestId);
+
+    bytes32 private jobId;
+    uint256 private fee;
+
+    constructor(string memory _name, string memory _ticker, uint256 _value, address _collateral, address _minter, string[] memory _tickers, uint256[] memory _weights) ERC4626 (ERC20(_collateral), _name, _ticker) {
+
+        // simple error handling
+
+        require(_value > 0, "No value");
+        require(_tickers.length <= 5, "Exceeded limit on number of underlying assets (5)");
+        require(_tickers.length == _weights.length, "Failed to match assets with weights");
+
+        // Check that weights add to 100
+        uint256 _totalWeight = 0;
+        for (uint256 i = 0; i < _weights.length; i++) {
+            _totalWeight += _weights[i];
+        }
+        require(_totalWeight == 100, "Bad weights");
+
+        // transfer collateral
+        deposit(_value, _minter);
+
+        // validate transfer
+        require(ERC20(_collateral).balanceOf(address(this)) == _value, "Failed to transfer collateral");
+
+        // set value
+         value = _value;
+
+        // price oracle
         
-        // TODO add error handling
-        // ^ ensure proper assets + weights
+        // reference: https://docs.chain.link/resources/link-token-contracts
+        _setChainlinkToken(0x779877A7B0D9E8603169DdbD7836e478b4624789); // sepolia
 
-        value = _value;
-        
-        collateral = ERC20(_collateral)
-        oracle = _oracle; // TODO figure out oracle
+        // reference: https://docs.chain.link/any-api/testnet-oracles
+        _setChainlinkOracle(0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD); // sepolia
 
-        collateral.transferFrom(_minter, address payable(this), value);
+        jobId = "7da2702f37fd48e5b1b9a5715e3509b6"; // GET > bytes
 
-        // TODO add error handling - ensure that balance is updated (or revert)
+        fee = (1 * LINK_DIVISIBILITY) / 10; 
 
-        for (uint i = 0; i < _assets.length; i++) {
-            assets.push(Asset({name: _assets[i], weight: _weights[i], amount: _weights[i]*value})); // TODO find an arithmetic library for safe math?
+        // set assets
+        for (uint i = 0; i < _tickers.length; i++) {
+            underlying_assets.push(Asset({ticker: _tickers[i], weight: _weights[i]}));
         }
 
     }
 
-    // TODO use price feed to update value of vault according to price movements of underlying assets
+    function getAssetValue(string memory _url, string memory _ticker) public {
+        Chainlink.Request memory request = _buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+
+        // api reference: https://finnhub.io/docs/api/quote
+        request._add("get", _url);
+        request._add("path", "c");
+
+        bytes32 requestId = _sendChainlinkRequest(request, fee);
+        query_asset[requestId] = _ticker;
+        emit RequestMade(requestId);
+    }
+
+    function fulfill(bytes32 requestId, bytes memory result) public recordChainlinkFulfillment(requestId) {
+        emit RequestProcessed(requestId);
+        underlying_asset_value[query_asset[requestId]] = abi.decode(result, (uint256));
+    }
+
+    function calculateSAValue() public returns (bool) {
+
+        if (underlying_asset_qty_is_empty) {
+            underlying_asset_qty_is_empty = !calculateQty();
+            return true;
+        }
+
+        uint256 _totalValue;
+        for (uint256 i = 0; i < underlying_assets.length; i++) {
+            string memory _ticker = underlying_assets[i].ticker;
+            _totalValue += underlying_asset_qty[_ticker] * underlying_asset_value[_ticker];
+        }
+        value = _totalValue;
+        return true;
+
+    }
+
+    function calculateQty() public returns (bool) {
+
+        for (uint256 i = 0; i < underlying_assets.length; i++) {
+            string memory _ticker = underlying_assets[i].ticker;
+            underlying_asset_qty[_ticker] = value*underlying_assets[i].weight / underlying_asset_value[_ticker];
+        }
+        return true;
+
+    }
+
+    /**
+     * @inheritdoc ERC4626
+     */
+    function totalAssets() public view override returns (uint256) {
+        assembly { // better safe than sorry
+            if eq(sload(0), 2) {
+                mstore(0x00, 0xed3ba6a6)
+                revert(0x1c, 0x04)
+            }
+        }
+        return asset.balanceOf(address(this));
+    }
     
     /**
      * @inheritdoc ERC4626
