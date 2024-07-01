91b61f33d61bff4a1ab619e3b1282c7c8eb22749
SabrinaHirani <sabrinahirani@gmail.com>
2024-07-01 10:42:38 -0400

    Contract Compiles

--- contracts/contracts/GradientDEX.sol
+++ contracts/contracts/GradientDEX.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "solmate/src/tokens/ERC20.sol";
 import "solmate/src/utils/FixedPointMathLib.sol";
+import "solmate/src/utils/ReentrancyGuard.sol";
 
-contract LiquidityRewardPool {
+contract LiquidityRewardPool is ReentrancyGuard {
 
     using FixedPointMathLib for uint256;
 
@@ -20,33 +21,32 @@ contract LiquidityRewardPool {
         timestamp = block.timestamp;
     }
 
-    function distribute() public {
+    function distribute() public nonReentrant {
 
         require(block.timestamp > timestamp + delay, "Too early");
 
         uint256 _totalReward = dai.balanceOf(address(this));
-        address[] providers = accounting.providers();
-        mapping (address => bool) memory rewarded;
+        address[] memory providers = accounting.getProviders();
 
         for (uint256 i = 0; i < providers.length; i++) {
-            if (rewarded[providers[i]] == false) {
-                uint256 _reward = _totalReward.mulWadDown(accounting.balanceOf(msg.sender)).divWadDown(accounting.totalSupply());
-                dai.transfer(providers[i], _reward);
-            }
+            uint256 _reward = _totalReward.mulWadDown(accounting.balanceOf(msg.sender)).divWadDown(accounting.totalSupply());
+            dai.transfer(providers[i], _reward);
         }
+
+        timestamp = block.timestamp;
     }
     
 }
 
-contract LiquidityPool is ERC20 {
+contract LiquidityPool is ERC20, ReentrancyGuard {
 
     using FixedPointMathLib for uint256;
 
     ERC20 immutable x; // gradient asset
     ERC20 immutable y; // dai
+    ERC20 immutable dai; // also dai
 
     address immutable reward; // reward pool
-    address immutable dai;
 
     uint256 public FEE = 1;
 
@@ -59,46 +59,46 @@ contract LiquidityPool is ERC20 {
         x.transferFrom(msg.sender, address(this), _amountX);
         y.transferFrom(msg.sender, address(this), _amountY);
         _mint(msg.sender, _amountY +  XToY(_amountX));
-        providers.append(provider);
+        providers.push(provider);
 
         reward = address(new LiquidityRewardPool(_y, address(this)));
         dai = ERC20(_y);
     }
 
-    function depositX(uint256 _amountX) public {
+    function depositX(uint256 _amountX) public nonReentrant {
         x.transferFrom(msg.sender, address(this), _amountX);
         uint _amountY = XToY(_amountX);
-        if (balanceOf(msg.sender) == 0) {
-            providers.append(msg.sender);
+        if (ERC20(this).balanceOf(msg.sender) == 0) {
+            providers.push(msg.sender);
         }
         _mint(msg.sender, _amountY);
     }
 
-    function depositY(uint256 _amountY) public {
+    function depositY(uint256 _amountY) public nonReentrant {
         y.transferFrom(msg.sender, address(this), _amountY);
-        if (balanceOf(msg.sender) == 0) {
-            providers.append(msg.sender);
+        if (ERC20(this).balanceOf(msg.sender) == 0) {
+            providers.push(msg.sender);
         }
         _mint(msg.sender, _amountY);
     }
 
-    function withdrawX(uint256 _amountX) public {
-        if (XToY(balanceOf(msg.sender)) < _amountX) {
-            _amountX = XToY(balanceOf(msg.sender));
+    function withdrawX(uint256 _amountX) public nonReentrant {
+        if (XToY(ERC20(this).balanceOf(msg.sender)) < _amountX) {
+            _amountX = XToY(ERC20(this).balanceOf(msg.sender));
         }
         x.transfer(msg.sender, _amountX);
         _burn(msg.sender, _amountX);
     }
 
-    function withdrawY(uint256 _amountY) public {
-        if (balanceOf(msg.sender) < _amountY) {
-            _amountY = balanceOf(msg.sender);
+    function withdrawY(uint256 _amountY) public nonReentrant {
+        if (ERC20(this).balanceOf(msg.sender) < _amountY) {
+            _amountY = ERC20(this).balanceOf(msg.sender);
         }
         y.transfer(msg.sender, _amountY);
         _burn(msg.sender, _amountY);
     }
 
-    function tradeXToY(uint256 _amountX) public {
+    function tradeXToY(uint256 _amountX) public nonReentrant {
 
         dai.transferFrom(msg.sender, address(reward), FEE);
 
@@ -108,7 +108,7 @@ contract LiquidityPool is ERC20 {
 
     }
 
-    function tradeYToX(uint256 _amountY) public {
+    function tradeYToX(uint256 _amountY) public nonReentrant {
 
         dai.transferFrom(msg.sender, address(reward), FEE);
 
@@ -117,12 +117,16 @@ contract LiquidityPool is ERC20 {
         x.transfer(msg.sender, _amountX);
     }
 
-    function XToY(uint256 _amountX) public returns (uint256) {
+    function XToY(uint256 _amountX) public view returns (uint256) {
         return _amountX.mulWadDown(y.balanceOf(address(this))).divWadDown(x.balanceOf(address(this)));
     }
 
-    function YToX(uint256 _amountY) public returns (uint256) {
+    function YToX(uint256 _amountY) public view returns (uint256) {
         return _amountY.mulWadDown(x.balanceOf(address(this))).divWadDown(y.balanceOf(address(this)));
+    }
+
+    function getProviders() public view returns (address[] memory) { // Added function to fetch providers
+        return providers;
     }
 
 }
@@ -131,9 +135,9 @@ contract GradientDEX {
 
     address[] public pools;
 
-    function registerLiquidityPool(address _x, address _y, uint256 _amountX, uint256 _amountY) {
+    function registerLiquidityPool(address _x, address _y, uint256 _amountX, uint256 _amountY) public {
         LiquidityPool lp = new LiquidityPool(_x, _y, _amountX, _amountY, msg.sender);
-        pools.append(address(lp));
+        pools.push(address(lp));
     }
 
 }
